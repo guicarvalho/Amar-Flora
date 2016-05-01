50727ed2abeed5e9754029044f3590e1004b5cce
Guilherme Arthur de Carvalho <Guilherme Arthur de Carvalho>
2016-05-01 18:47:19 -0300

    implements i18n

--- core/admin.py
+++ core/admin.py
@@ -3,11 +3,26 @@
 
 from django.contrib import admin
 
-from core.models import News
+from core.models import News, Partner, Classified, Request
 
 
 class NewsAdmin(admin.ModelAdmin):
 	pass
 
 
+class PartnerAdmin(admin.ModelAdmin):
+	pass
+
+
+class ClassifiedAdmin(admin.ModelAdmin):
+	pass
+
+
+class RequestAdmin(admin.ModelAdmin):
+	pass
+
+
 admin.site.register(News, NewsAdmin)
+admin.site.register(Partner, PartnerAdmin)
+admin.site.register(Classified, ClassifiedAdmin)
+admin.site.register(Request, RequestAdmin)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 from django.db import models
 
@@ -8,14 +8,21 @@ from django.core.validators import MinValueValidator
 
 
 class Partner(models.Model):
-	name = models.CharField(max_length=50)
-	phone = models.CharField(max_length=11, blank=True, null=True)
-	email = models.EmailField()
-	benefit = models.CharField(max_length=100)
+	name = models.CharField(_('Name'), max_length=50)
+	phone = models.CharField(_('Phone'), max_length=11, blank=True, null=True)
+	email = models.EmailField(_('E-mail'))
+	benefit = models.CharField(_('Benefit'), max_length=100)
+
+	class Meta:
+		verbose_name = _('Partner')
+		verbose_name_plural = _('Partners')
+
+	def __str__(self):
+		return self.name
 
 
 class Associate(models.Model):
-	name = models.CharField(_('Name'), max_length=50, help_text=_('Enter your name'))
+	name = models.CharField(_('Name'), max_length=50)
 	rg = models.CharField(_('RG'), max_length=10)
 	cpf = models.CharField(_('CPF'), max_length=11)
 	birthday = models.DateField(_('Birthday'))
@@ -44,15 +51,46 @@ class News(models.Model):
 
 
 class Request(models.Model):
-	email = models.EmailField()
-	name = models.CharField(max_length=50)
-	phone = models.CharField(max_length=11)
-	street = models.CharField(max_length=100)
-	number = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(1)])
-	complement = models.CharField(max_length=35, blank=True, null=True)
-	description = models.TextField()
-	status = models.CharField(max_length=1)
-	subject = models.IntegerField()
+	OTHERS = 1
+	SECURITY = 2
+	LIGHTING = 3
+	CONSTRUCTION = 4
+	CLEANING = 5
+
+	SUBJECT_CHOICES = (
+		(OTHERS, _('Others')),
+		(SECURITY, _('Security')),
+		(LIGHTING, _('Lighting')),
+		(CONSTRUCTION, _('Construction')),
+		(CLEANING, _('Cleaning')),
+	)
+
+	OPEN = 'O'
+	WAITING = 'W'
+	CLOSED = 'C'
+
+	STATUS_CHOICE = (
+		(OPEN, _('Open')),
+		(WAITING, _('Waiting')),
+		(CLOSED, _('Closed')),
+	)
+
+	email = models.EmailField(_('E-mail'))
+	name = models.CharField(_('Name'), max_length=50)
+	phone = models.CharField(_('Phone'), max_length=11)
+	street = models.CharField(_('Street'), max_length=100)
+	number = models.IntegerField(_('Number'), null=True, blank=True, validators=[MinValueValidator(1)])
+	complement = models.CharField(_('Complement'), max_length=35, blank=True, null=True)
+	description = models.TextField(_('Description'))
+	status = models.CharField(_('Status'), max_length=1, choices=STATUS_CHOICE, default=OPEN)
+	subject = models.IntegerField(_('Subject'), choices=SUBJECT_CHOICES)
+
+	class Meta:
+		verbose_name = _('Request')
+		verbose_name_plural = _('Requests')
+
+	def __str__(self):
+		return self.name
 
 
 class Gallery(models.Model):
@@ -62,5 +100,13 @@ class Gallery(models.Model):
 
 
 class Classified(models.Model):
-	title = models.CharField(max_length=80)
-	description = models.TextField()
+	title = models.CharField(_('Title'), max_length=80)
+	description = models.TextField(_('Description'))
+	site = models.URLField(_('Site'))
+
+	class Meta:
+		verbose_name = _('Classified')
+		verbose_name_plural = _('Classifieds')
+
+	def __str__(self):
+		return self.title
