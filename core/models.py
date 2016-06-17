517860c53d46d574f2407c10ad6f075ddaefd883
Giovanna Franco <gui.ifsp11@gmail.com>
2016-06-17 17:54:20 -0300

    mudanças banco

--- core/models.py
+++ core/models.py
@@ -178,7 +178,6 @@ class Association_information(models.Model):
     name = models.CharField(_('Name'), max_length=80)
     cnpj = models.CharField(_('CNPJ'), max_length=18)
     email = models.EmailField()
-    short_description = models.CharField(_('Short description'), max_length=100, blank=True, null=True)
     long_description = models.TextField(_('Long Description'))
     home_description = models.TextField(_('Home Description'))
 
@@ -192,8 +191,8 @@ class Association_information(models.Model):
 
 class UsefulPhone(models.Model):
     name = models.CharField(_('Name'), max_length=80)
-    phone1 = models.CharField(_('Phone'), max_length=11)
-    phone2 = models.CharField(_('Phone'), max_length=11, blank=True, null=True)
+    phone1 = models.CharField(_('Phone'), max_length=12)
+    phone2 = models.CharField(_('Phone'), max_length=12, blank=True, null=True)
 
     class Meta:
         verbose_name = _('Useful_phone')
