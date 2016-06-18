a83186d5b6dd9dbb38c13781e170f09d5f34114c
Giovanna Franco <gui.ifsp11@gmail.com>
2016-06-17 22:55:02 -0300

    ajuste no setting

--- amar_flora/settings.py
+++ amar_flora/settings.py
@@ -80,13 +80,8 @@ WSGI_APPLICATION = 'amar_flora.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'amar_flora',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
